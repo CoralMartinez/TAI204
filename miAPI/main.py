6789a7203c3d78bd61bef1fe2b32bdde6cd28ef9
CoralMartinez <coralmtzs084@gmail.com>
2026-01-29 15:23:24 -0600

    practica_1

---
+++ miAPI/main.py
@@ -0,0 +1,50 @@
+#importaciones
+
+
+#librería: fastapi, clase: FastAPI
+
+import asyncio  
+
+
+#Instancia del servidor
+
+app =  FastAPI()
+
+
+#Endpoints
+
+#--->Rutas
+
+#PRIMERA FUNCIÓN
+@app.get("/")
+
+#función asincrona: bienvenido
+
+async def bienvenido():
+    
+    return {"mensaje":"Bienvenido a FastAPI "}
+
+#JSON, REGRESA: IZQUIERDA: Clave/index  DERECHA: valor del mensaje
+#{"correo":"correo@ejemplo.com"}
+
+#SEGUNDA FUNCIÓN
+
+@app.get("/holaMundo")
+
+async def Hola():
+    
+    await asyncio.sleep(5)  # Simulación de una operación asíncrona
+    #await---> petición, consulta a una BD, archivo
+    
+    return {
+        "mensaje":"Hola Mundo FastAPI",
+        "status":"200"
+        }
+    
+
+
+
+
+
+
+
