eb7739aa67e962f5ea904ad45144e821aa77a569
CoralMartinez <coralmtzs084@gmail.com>
2026-02-26 09:26:30 -0600

    practica 4 terminada

--- miAPI/app/main.py
+++ miAPI/app/main.py
@@ -8,7 +8,7 @@ import asyncio
 
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 from fastapi.middleware.cors import CORSMiddleware
@@ -50,9 +50,9 @@ usuarios = [
 #-------------------------------------------------------------------------------------------
 
 class crear_usuario(BaseModel):
-    id:int
-    nombre:str
-    edad:int
+    id:int = Field(...,gt=0, description="identificador  de usuario")
+    nombre:str = Field(...,min_length=3, max_length=50, example="Patroclo")
+    edad:int = Field(...,ge=1, le=125, description="Edad valida de 1 a 125")
 
 
 #Endpoints
